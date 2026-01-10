4d74bb3a40fce70220a948c4adc797fb5da63573
rahulguptahack <rahulguptahack@gmail.com>
2026-01-10 23:06:01 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,8 @@
 const products = [
-  { id: 1, name: "Basmati Rice 5 Kg", price: 300, img: "https://via.placeholder.com/100" },
+  { id: 1, name: "Basmati Rice 5 Kg", price: 300, img: "img: "images/rice.jpg"" },
   { id: 2, name: "Wheat Atta 10 Kg", price: 420, img: "https://via.placeholder.com/100" },
   { id: 3, name: "Cooking Oil 1 L", price: 160, img: "https://via.placeholder.com/100" },
   { id: 4, name: "Sugar 1 Kg", price: 45, img: "https://via.placeholder.com/100" },
   { id: 5, name: "Tea 500 g", price: 180, img: "https://via.placeholder.com/100" }
 ];
+
