cacf699c39664b44c8d7aad4ec2c744d85ba664d
rahulguptahack <rahulguptahack@gmail.com>
2026-01-11 11:05:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,30 @@ products.forEach(p => {
       <img src="./${p.img}" alt="${p.name}">
       <h4>${p.name}</h4>
       <p>₹${p.price}</p>
+
+      <div class="qty">
+        <button onclick="changeQty(${p.id}, -1)">−</button>
+        <span id="qty-${p.id}">1</span>
+        <button onclick="changeQty(${p.id}, 1)">+</button>
+      </div>
+
       <button onclick="addToCart(${p.id})">Add to Cart</button>
     </div>
   `;
 });
 
+let quantities = {};
+
+function changeQty(id, delta) {
+  quantities[id] = (quantities[id] || 1) + delta;
+  if (quantities[id] < 1) quantities[id] = 1;
+  document.getElementById(`qty-${id}`).innerText = quantities[id];
+}
+
 function addToCart(id) {
-  const item = products.find(p => p.id === id);
-  cart.push(item);
+  const product = products.find(p => p.id === id);
+  const qty = quantities[id] || 1;
+
+  cart.push({ ...product, qty });
   document.getElementById("cartCount").innerText = cart.length;
 }
